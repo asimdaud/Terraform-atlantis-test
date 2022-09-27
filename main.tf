8a89c3b1b0f6757dc8d3248e8dc26b4ba3755b64
Asim <45770583+asimdaud@users.noreply.github.com>
2022-09-27 14:36:43 +0100

    create vpc

--- main.tf
+++ main.tf
@@ -2,27 +2,37 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+terraform {
+  backend "s3" {
+    encrypt = true    
+    bucket = "asim-atlantis-test-tfstate"
+    dynamodb_table = "terraform-lock"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
 # resource "null_resource" "cluster" {}
 
 
-################################################################################
+###############################################################################
 # VPC Module
-################################################################################
+###############################################################################
 
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-#   name = "atlantis-vpc"
-#   cidr = "10.50.0.0/16"
+  name = "atlantis-vpc"
+  cidr = "10.50.0.0/16"
 
-#   enable_nat_gateway = false
+  enable_nat_gateway = false
 
-#   tags = {
-#     Owner       = "user"
-#     Environment = "dev"
-#   }
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
 
-#   vpc_tags = {
-#     Name = "vpc-atlantis"
-#   }
-# }
+  vpc_tags = {
+    Name = "vpc-atlantis"
+  }
+}
