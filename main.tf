0c2f0c5d8a418f89a29be64c946869b8a893449c
Asim <45770583+asimdaud@users.noreply.github.com>
2022-09-27 14:19:53 +0100

    test vpc

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
-resource "null_resource" "cluster" {}
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# resource "null_resource" "cluster" {}
+
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "atlantis-vpc"
+  cidr = "10.50.0.0/16"
+
+  enable_nat_gateway = false
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-atlantis"
+  }
+}
